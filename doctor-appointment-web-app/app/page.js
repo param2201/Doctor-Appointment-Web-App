b24d448077131649fa80a89d0b2adcaecbf6925d
Param Mistry <parammistryo3@gmail.com>
2024-06-18 16:51:52 +0530

    added button using shadcnn

--- doctor-appointment-web-app/app/page.js
+++ doctor-appointment-web-app/app/page.js
@@ -1,9 +1,11 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
       <h2>My First Website</h2>
+      <Button>Login</Button>
     </div>
   );
 }
